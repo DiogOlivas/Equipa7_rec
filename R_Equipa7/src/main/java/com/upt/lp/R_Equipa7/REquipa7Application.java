f20c429fdeef33c8a3247de606708e40b36fcaf7
Diogo Oliveira <52390@alunos.upt.pt>
2025-12-12 12:01:25 +0000

    Estrutura Springboot

---
+++ R_Equipa7/src/main/java/com/upt/lp/R_Equipa7/REquipa7Application.java
@@ -0,0 +1,13 @@
+package com.upt.lp.R_Equipa7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class REquipa7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(REquipa7Application.class, args);
+	}
+
+}
---
+++ R_Equipa7/src/test/java/com/upt/lp/R_Equipa7/REquipa7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upt.lp.R_Equipa7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class REquipa7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
